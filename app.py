d6a30f3f04c417c045e944b4cbc70949a9b39807
Jared Tauler <81237109+JaredTauler@users.noreply.github.com>
2022-04-07 15:03:11 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,262 @@
+# Jared Tauler 3/29/22
+import json
+import base64
+import os
+import yaml
+import hashlib
+import time
+import qrcode
+
+# flask
+from flask import Flask, abort, redirect, url_for, request, session, render_template, jsonify, make_response
+from flask_session import Session
+from sqlalchemy_imageattach.entity import store_context
+from sqlalchemy_imageattach.stores.fs import HttpExposedFileSystemStore
+
+# from sqlalchemy_imageattach.entity import store_context
+# import sqlalchemy_imageattach
+
+### Prepare app
+app = Flask(__name__)
+app.secret_key = os.urandom(16)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tes3t.db'
+app.config['SESSION_TYPE'] = "filesystem"
+
+### DATABASE
+import database as db
+
+store = HttpExposedFileSystemStore('image', 'image/')
+app.wsgi_app = store.wsgi_middleware(app.wsgi_app)
+# app.wsgi_app = fs_store.wsgi_middleware(app.wsgi_app)
+
+
+NoLoginWhitelist = [
+	"/login"
+
+]
+
+# @app.before_request
+# def guide ():
+# 	if request.path == "/":  # If at root, redirect
+# 		return redirect(url_for("Home"))
+# 	if not session.get("id"):  # If not logged in,
+# 		if not request.path in NoLoginWhitelist:  # If accessing a whitelisted route,
+# 			args = request.args
+# 			arguments = ""
+# 			for key, val in zip(args.keys(), args.values()):
+# 				arguments += f"{key}={val}&"
+# 			if arguments != "":
+# 				arguments = "?" + arguments[:-1]
+# 			string = f"#{request.path[1:]}{arguments}"  # Redirect to path user was trying to access + remember
+# 			# arguments.
+# 			print(string)
+# 			return redirect(url_for("Login") + string)
+
+### ROUTES
+@app.template_global()
+def static_include (filename):
+	fullpath = os.path.join(app.static_folder, filename)
+	with open(fullpath, 'r') as f:
+		return f.read()
+
+@app.route('/logout', methods=["POST"])
+def Logout ():
+	# No need to check if given session has id. guide() should take care of that.
+	session.pop("id")
+	return "", 200
+
+@app.route('/login', methods=["GET", "POST"])
+def Login ():
+	# session.pop("id")
+	if request.method == "GET":
+		return render_template("login.html")
+	else:
+		rd = request.form.to_dict()  # Get form data.
+		row = db.User.query.filter_by(username=rd.get("username")).first()  # Get user from DB..
+		if not row:  # Couldnt find user in DB.
+			jsonify(["redirect", "username"]), 200
+		if password(rd.get("password"), row.salt)[
+			0] == row.hashed:  # Check given password hashed is same as one in database.
+			session["id"] = row.id
+			return jsonify(["pass", 0]), 200
+		# Password didnt match
+		return jsonify(["bad", "password"]), 200
+
+# sub = (
+# 	db.session.query(db.Ticket).
+# 		subquery()
+# )
+
+# usertickets = (
+# 	db.session.query(db.Ticket.quest_id)
+# 		.join(db.Quest)
+# 		.filter(db.Ticket.user_id == 1)
+# )
+# q = (
+# 	db.session.query(db.Ticket)
+# 		.filter(db.Ticket.quest_id == 2)
+# 		.filter(db.Ticket.user_id == 1)
+# 		.delete()
+# )
+
+
+# js = json.dumps(
+# 	{"user": usertickets},
+# 	cls=db.JsonEncoder
+# )
+# print(js)
+
+@app.route('/home', methods=["GET", "POST"])
+def Home ():
+	(
+		db.session.query(db.Ticket)
+			.filter(db.Ticket.quest_id == 1)
+			.filter(db.Ticket.user_id == 1)
+			.delete()
+	)
+	db.session.commit()
+	if request.method == "GET":
+		return render_template("home.html")
+	else:
+		# rd = request.form.to_dict()  # Get form data
+		rd = request.get_json()
+		if rd["intent"] == "get_info":
+			# Get all a user's tickets
+			usertickets = (
+				db.session.query(db.Ticket, db.Quest, )
+					.outerjoin(db.Quest, db.Ticket.quest_id == db.Quest.id)
+					.filter(db.Ticket.user_id == 1)
+			).all()
+			js = json.dumps(
+				{"user": usertickets},
+				cls=db.JsonEncoder
+			)
+			print(js)
+			return js, 200
+		return "bruh"
+
+@app.route("/quest", methods=["GET", "POST"])
+def TurnInQuest ():
+	if request.method == "GET":
+		return render_template("quest.html")
+	else:
+		(
+			db.session.query(db.Ticket)
+				.filter(db.Ticket.quest_id == request.args["id"])
+				.filter(db.Ticket.user_id == 1)
+				.delete()
+		)
+
+		ticket = db.Ticket(
+			user_id=1,  # session["id"],
+			quest_id=request.args["id"]
+		)
+		db.session.add(ticket)
+		with store_context(store):
+			ticket.picture.from_file(request.files["ImageUpload"])
+			print(ticket.picture)  # FIXME This crashes if this isnt here. No idea why.
+
+		db.session.commit()
+
+		new = db.TicketNew(
+			id=ticket.id
+		)
+		db.session.add(new)
+		db.session.commit()
+		return "", 200
+
+@app.route("/process_ticket", methods=["GET", "POST"])
+def Process_Ticket ():
+	if request.method == "GET":
+		return render_template("process_ticket.html")
+	else:
+		rd = request.get_json()
+		CurrentTicket = rd["current_ticket"]
+
+		if CurrentTicket:
+			if rd["decision"] == "accept":
+				try:
+					new = db.TicketGood(id=int(CurrentTicket))
+					db.session.add(new)
+				except Exception as e:
+					db.session.rollback()
+					print(e)
+					return "", 500
+
+			elif rd["decision"] == "reject":
+				db.Ticket.query.filter(db.Ticket.id == int(CurrentTicket)).delete()
+
+			db.TicketNew.query.filter(db.TicketNew.id == int(CurrentTicket)).delete()
+			db.session.commit()
+
+		if rd["intent"] == "get-ticket":
+			row = db.Ticket.query.filter(db.Ticket.id == db.TicketNew.id).first()
+			if row:
+				with store_context(store):
+					url = row.picture.locate()
+				user = db.User.query.filter(db.User.id == row.user_id).first()
+				quest = db.Quest.query.filter(db.Quest.id == row.quest_id).first()
+				print(user)
+				return jsonify(
+					{
+						"picture-url": url,
+						"ticket"     : {
+							"id": row.id
+						},
+						"user"       : {
+							"username": "test"  # user.username
+						},
+						"quest"      : {
+							"name": quest.name,
+							"task": quest.task
+						}
+					}
+				)
+			else:  # No more rows
+				return '', 204
+
+@app.route("/quest_manage", methods=["GET", "POST"])
+def QuestManage ():
+	if request.method == "GET":
+		return render_template("quest_manage.html")
+	else:
+		rd = request.form.to_dict()
+		if rd['intent'] == "save":
+			row = db.Quest.query.filter(db.Quest.id == rd["id"]).first()
+
+			DictToRow(row, rd)
+
+			db.session.commit()
+			return jsonify(""), 200
+		elif rd['intent'] == "new":
+			new = db.Quest()
+			DictToRow(new, rd)
+			db.session.add(new)
+			db.session.commit()
+			return jsonify(""), 200
+
+@app.route("/quest_manage/ajax", methods=["GET"])
+def QuestManageAjax ():
+	if request.method == "GET":
+		data = json.dumps(db.Quest.query.all(), cls=db.JsonEncoder)
+		return data, 200
+
+def DictToRow (row, dict):
+	# This can probably be SQL injected. Only administrators can access this page so not a problem.
+	for key, value in dict.items():
+		if hasattr(row, key):
+			setattr(row, key, value)
+
+def password (password, salt=os.urandom(32)):
+	hashed = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+	return hashed, salt
+
+hashed, salt = password("a")
+new = db.User(id="21", username="jared", hashed=hashed, salt = salt)
+db.session.add(new)
+db.session.commit()
+
+
+### RUN
+if __name__ == '__main__':
+	app.run()
---
+++ database.py
@@ -0,0 +1,98 @@
+import sqlalchemy.engine
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_imageattach.entity import Image, image_attachment, store_context
+
+from app import app  # Bad practice
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	# __tablename__ = "user"
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(80), unique=True, nullable=False)
+	# email = db.Column(db.String(120), unique=True, nullable=False)
+	hashed = db.Column(db.String(120), unique=False, nullable=False)
+	salt = db.Column(db.String(120), unique=False, nullable=False)
+
+class Worker(User):
+	id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+	__mapper_args__ = {
+		'polymorphic_identity': 'worker',
+	}
+
+class Quest(db.Model):
+	# __tablename__ = "quest"
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String(80), nullable=False)
+	task = db.Column(db.String(80), nullable=False)
+	info = db.Column(db.String(80), nullable=False)
+
+class Ticket(db.Model):
+	__tablename__ = "ticket"
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	quest_id = db.Column(db.Integer, db.ForeignKey('quest.id'))
+	# picture = image_attachment('TicketPicture')
+	worker_id = db.Column(db.Integer, db.ForeignKey('worker.id'), nullable=True)
+
+	pictures = db.relationship("TicketPicture", cascade="all,delete",)
+# quest = db.Column(db.Model.Quest, nullable=False)
+# email = db.Column(db.String(120), unique=True, nullable=False)
+
+class TicketGood(db.Model):
+	id = db.Column(db.Integer, db.ForeignKey('ticket.id'), primary_key=True)
+	__mapper_args__ = {
+		'polymorphic_identity': 'ticketgood',
+	}
+
+class TicketNew(db.Model):
+	id = db.Column(db.Integer, db.ForeignKey('ticket.id'), primary_key=True)
+	__mapper_args__ = {
+		'polymorphic_identity': 'ticketnew',
+	}
+
+class TicketPicture(db.Model, Image):
+	__tablename__ = "ticketpicture"
+	"""User picture model."""
+	ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'), primary_key=True)
+	ticket = db.relationship(
+		"Ticket",
+		backref=db.backref("pictures", cascade="all, delete-orphan")
+		)
+
+# ticket = db.relationship('Ticket')
+# id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+# __tablename__ = 'user_picture'
+
+db.create_all()
+db.session.commit()
+
+session = db.session
+
+import json
+from sqlalchemy.ext.declarative import DeclarativeMeta
+
+class JsonEncoder(json.JSONEncoder):
+	def default (self, obj):
+		def meta(obj):
+			fields = {}
+			for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+				data = obj.__getattribute__(field)
+				try:
+					json.dumps(data)  # this will fail on non-encodable values, like other classes
+					fields[field] = data
+				except TypeError:
+					fields[field] = None
+			# a json-encodable dict
+			return fields
+		print("bruh")
+		if obj.__class__ == sqlalchemy.engine.row.Row:
+			tables = {}
+			for t in obj:
+				tables[t.__table__.name] = meta(t)
+			return tables
+
+		if isinstance(obj.__class__, DeclarativeMeta):
+			return meta(obj)
+
+		return json.JSONEncoder.default(self, obj)
